51dc7e154cd86a7518a99951cdbdd82f52f2edfe
slothman3878 <sloth.man3878@gmail.com>
2024-03-17 23:40:28 -0700

    vq-vae

--- data_loader.py
+++ data_loader.py
@@ -4,9 +4,9 @@ import torch.utils.data.sampler as sampler
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
-def load_data(batch_size=100, flipped=False, outline=False, normalize=False):
+def load_mnist(batch_size=100, flipped=False, outline=False, normalize=False):
     train_dataset = datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
-    test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+    test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor(), download=True)
 
     if flipped:
         train_dataset = list(map(lambda x: (abs(1-x[0]), x[1]), train_dataset))
@@ -18,6 +18,28 @@ def load_data(batch_size=100, flipped=False, outline=False, normalize=False):
         train_dataset = list(map(lambda x: (1-(abs(.5-x[0]) * 2), x[1]), train_dataset))
         test_dataset = list(map(lambda x: (1-(abs(.5-x[0]) * 2), x[1]), test_dataset))
 
+    train_loader = data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+    indices = range(len(test_dataset))
+    indices_val = indices[:5000]
+    data.TensorDataset()
+    indices_test = indices[5000:]
+
+    sampler_val = sampler.SubsetRandomSampler(indices_val)
+    sampler_test = sampler.SubsetRandomSampler(indices_test)
+
+    # not sure if there is any benefit to batching validation and test data
+    validation_loader = data.DataLoader(dataset=test_dataset, batch_size=1, shuffle=False,
+                                                    sampler=sampler_val)
+    test_loader = data.DataLoader(dataset=test_dataset, batch_size=1, shuffle=False,
+                                              sampler=sampler_test)
+
+    return train_loader, validation_loader, test_loader
+
+def load_cifar10(batch_size=100):
+    train_dataset = datasets.CIFAR10(root='./data/cifar10', train=True, transform=transforms.ToTensor(), download=True)
+    test_dataset = datasets.CIFAR10(root='./data/cifar10', train=False, transform=transforms.ToTensor())
+    
     train_loader = data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
     indices = range(len(test_dataset))
